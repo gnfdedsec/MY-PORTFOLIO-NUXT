32a6054de978d7db32130fec5dc6df8b650594de
gnfdedsec <ssuraw@kku.ac.th>
2024-10-20 16:00:38 +0700

    commit

---
+++ pages/projecshowcase.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+      <ProjectShowcase />
+    </div>
+  </template>
+  
+  <script>
+  import ProjectShowcase from './components/ProjectShowcase.vue';
+  
+  export default {
+    components: {
+      ProjectShowcase
+    }
+  };
+  </script>
+  
+  <style>
+  /* สไตล์เพิ่มเติมถ้าจำเป็น */
+  </style>
