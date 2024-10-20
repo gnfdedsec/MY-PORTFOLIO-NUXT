3bad1ab158f1ef7cdf29a32883d6f29adf3a6892
gnfdedsec <ssuraw@kku.ac.th>
2024-10-20 16:01:39 +0700

    commit

--- pages/projecshowcase.vue
+++ pages/projecshowcase.vue
@@ -2,18 +2,23 @@
     <div id="app">
       <ProjectShowcase />
     </div>
-  </template>
-  
-  <script>
-  import ProjectShowcase from './components/ProjectShowcase.vue';
-  
-  export default {
-    components: {
-      ProjectShowcase
-    }
-  };
-  </script>
-  
-  <style>
-  /* สไตล์เพิ่มเติมถ้าจำเป็น */
-  </style>
+</template>
+
+<script>
+import ProjectShowcase from './components/ProjectShowcase.vue';
+
+export default {
+  components: {
+    ProjectShowcase
+  }
+};
+</script>
+
+<style>
+/* เพิ่มสไตล์ที่ต้องการ */
+#app {
+  /* ตัวอย่างการเพิ่มสไตล์ */
+  background-color: #f0f0f0;
+  padding: 20px;
+}
+</style>
