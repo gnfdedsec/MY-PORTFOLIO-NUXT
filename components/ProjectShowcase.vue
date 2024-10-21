67a66b0bdc1a88f1da37c14ff7c43009d4af9446
gnfdedsec <ssuraw@gmail.com>
2024-10-21 15:08:26 +0700

    commit

--- components/ProjectShowcase.vue
+++ components/ProjectShowcase.vue
@@ -29,8 +29,8 @@ const projects = ref([
   {
     name: 'ENKKU Donations',
     image: 'https://images.unsplash.com/photo-1567427017947-545c5f8d16ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-    detail: 'ระบบบริจาคออนไลน์สำหรับสนับสนุนกองทุนคณะ',
-    technologies: ['PHP', 'Bulma', 'MySQL', 'Vanilla JS'] 
+    detail: 'Online donation system for supporting faculty funds',
+    technologies: ['PHP', 'Bulma', 'MySQL', 'Vanilla JS']
   },
   {
     name: 'HRM Realtime',
