9be30b6c5be0bf15061369fa6fbb6af989b9c490
gnfdedsec <ssuraw@gmail.com>
2024-10-21 13:29:13 +0700

    test

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
   <div>
     <MainContent />
     <ShootingStarsBg> <div class="w-full "> 0 </div> </ShootingStarsBg>
-    <h1 class="text-3xl sm:text-4xl md:text-4xl mt-1 font-bold text-center">
-      MY <span class="bg-gradient-to-r from-green-400 via-fuchsia-500 to-orange-500 bg-[length:100%_6px] bg-no-repeat bg-bottom font-mono text-6xl sm:text-4xl md:text-3xl">SKILLS</span>
-    </h1>
+    
   </div>
 </template>
 
