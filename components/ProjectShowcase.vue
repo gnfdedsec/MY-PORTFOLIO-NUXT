73b5ab2fdc7b99239bc92d46fb52c0b2c90a5c6a
gnfdedsec <ssuraw@gmail.com>
2024-10-21 14:25:28 +0700

    xxx

--- components/ProjectShowcase.vue
+++ components/ProjectShowcase.vue
@@ -28,7 +28,7 @@ const projects = ref([
   },
   {
     name: 'ENKKU Donations',
-    image: 'https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+    image: 'https://i.postimg.cc/SsWZqSJf/Screenshot-2567-10-21-at-14-13-27.png',
     detail: 'Online Donation System for Faculty Fund Support',
     technologies: ['PHP', 'Bulma', 'MySQL', 'Vanilla JS'] 
   },
