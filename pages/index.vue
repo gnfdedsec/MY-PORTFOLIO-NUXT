50d13557b366e1508bc6bc89b2cd24c68e38f6e1
gnfdedsec <ssuraw@gmail.com>
2024-10-21 13:22:17 +0700

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-   
-      <MainContent />
-      <ShootingStarsBg> <div class="w-full "> 0 </div> </ShootingStarsBg>
+    <MainContent />
+    <ShootingStarsBg> <div class="w-full "> 0 </div> </ShootingStarsBg>
     <FavoriteQuote />
     <h1 class="text-3xl sm:text-4xl md:text-4xl mt-1 font-bold text-center">
       MY <span class="bg-gradient-to-r from-green-400 via-fuchsia-500 to-orange-500 bg-[length:100%_6px] bg-no-repeat bg-bottom font-mono text-6xl sm:text-4xl md:text-3xl">SKILLS</span>
     </h1>
-   <SkillSet />
-   
+    <SkillSet />
   </div>
 </template>
 
