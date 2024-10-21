6c8481982ff12d6f11e5e0429280f9af816100cd
gnfdedsec <ssuraw@gmail.com>
2024-10-21 13:58:35 +0700

    sett

--- components/ProjectShowcase.vue
+++ components/ProjectShowcase.vue
@@ -21,8 +21,8 @@ import { ref } from 'vue'
 
 const projects = ref([
   {
-    name: 'CVS Specialist',
-    image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+    name: 'CVS Interest Research',
+    image: 'https://cvs.enit.kku.ac.th/cvsenkku.svg',
     detail: 'Personnel Management System for ENKKU Academic Staff',
     technologies: ['Next.js', 'Tailwind CSS', 'JWT API', 'MySQL']
   },
