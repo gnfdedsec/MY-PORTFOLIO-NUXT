cbd70d456f05656a35e5ad4ff09d87246e5926ac
gnfdedsec <ssuraw@gmail.com>
2024-10-22 03:14:36 +0700

    mobile

--- components/ProjectShowcase.vue
+++ components/ProjectShowcase.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="p-6 sm:p-6 md:p-6 lg:p-6 p-0">
+  <div class="sm:p-6 md:p-6 lg:p-6 p-0">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       <div v-for="project in projects" :key="project.name" class="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg w-full">
         <img :src="project.image" :alt="project.name" class="w-full h-48 object-cover">
-        <div class="p-4 sm:p-4 md:p-4 lg:p-4 p-2">
+        <div class="sm:p-4 md:p-4 lg:p-4 p-2">
           <h3 class="text-xl font-semibold mb-2">{{ project.name }}</h3>
           <p class="text-gray-600 text-sm mb-4">{{ project.detail }}</p>
           <div class="flex flex-wrap gap-2 mb-4">
